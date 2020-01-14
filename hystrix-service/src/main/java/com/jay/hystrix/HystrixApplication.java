ae23b036cb43d7a237dfddb88f28473d20b2beaf
jay <1021038907@qq.com>
2020-01-15 00:09:30 +0800

    springcloud的断路器模块。

---
+++ hystrix-service/src/main/java/com/jay/hystrix/HystrixApplication.java
@@ -0,0 +1,15 @@
+package com.jay.hystrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+public class HystrixApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixApplication.class,args);
+    }
+}
